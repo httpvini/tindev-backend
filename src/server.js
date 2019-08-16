82ff3843ab54790442ac70e35bf6af988fa9f5a2
Marcus Oliveira <marcus.oliveira@m4u.com.br>
2019-08-15 21:30:40 -0300

    Criação da estrutura do projeto

--- src/server.js
+++ src/server.js
@@ -1,4 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose')
+const routes = require('./routes');
 const server = express();
+
+const dbConnection = 'mongodb+srv://omnistackdev:omnistackdev@cluster0-dpgyi.mongodb.net/omnistack8?retryWrites=true&w=majority'
+mongoose.connect(dbConnection, {
+    useNewUrlParser: true
+});
+
+server.use(express.json());
+server.use(routes);
 
 server.listen(3000, console.log("running on port 3000"));
