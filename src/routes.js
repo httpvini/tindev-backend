1cea26244a2042a99f4b49f8b4a44c7fa9d9a819
Vinicius Oliveira <vinicius.olliveira@outlook.com>
2019-08-17 21:48:23 -0300

    Cria recursos de like e dislike.

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const likeController = require('./controllers/likeController');
+const dislikeController = require('./controllers/dislikeController');
 const devController = require('./controllers/devController');
 const routes = express.Router();
 
 routes.post('/devs', devController.store);
+routes.post('/devs/:devId/likes', likeController.store);
+routes.post('/devs/:devId/dislikes', dislikeController.store);
 
 module.exports = routes;
